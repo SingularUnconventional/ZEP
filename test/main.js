e6adec464b6f93d9571c17352169def701b9f398
SnU <singularunconventional@gmail.com>
2023-07-12 04:07:25 +0900

    20230712_start

--- test/main.js
+++ test/main.js
@@ -1,30 +1,31 @@
-let a = 0;
-let b = 0;
+let a = null;
+let bud = 0;
 //특정 숫자 제시하면 그 숫자를 빨리 맞추는 사람 승리
 App.addOnKeyDown(82, function(player){
     if(player.tileY == 10)
-        b = player.tileX - 10;
+    {
+        if(bud == 0) a += player.tileX - 10;
+        if(bud == 1) a -= player.tileX - 10;
+        if(bud == 2) a *= player.tileX - 10;
+        if(bud == 3) a /= player.tileX - 10;
+    }
+        
     if(player.tileY == 12)
     {
-        if(player.tileX == 10)
-            a += b;
-
-        if(player.tileX == 12)
-            a *= b;
+        if(player.tileX == 10) bud = 0;
+        if(player.tileX == 12) bud = 2;
 
         if(player.tileX == 19)
         {
             App.sayToAll(`값은 : ${a}`); 
             a = 0;
+            bud = 0;
         }
     }
     if(player.tileY == 13)
     {
-        if(player.tileX == 10)
-            a -= b;
-
-        if(player.tileX == 12)
-            a /= b;
+        if(player.tileX == 10) bud = 1;
+        if(player.tileX == 12) bud = 3;
     }
 	
 });
